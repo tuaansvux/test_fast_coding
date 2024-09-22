43029239a9a97ad98b1de2252faaf4b790675e46
vuho <hotuanvu1234@gmail.com>
2024-09-22 12:59:47 +0700

    update dev

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
